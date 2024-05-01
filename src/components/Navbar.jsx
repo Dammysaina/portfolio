0b13e01c80bdf6dc444f64c78ff656115b61c00d
dammysaina <dammyadediran94@gmail.com>
2024-05-02 00:26:49 +0100

    home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/images/logo1.png";
 
 const Navbar = () => {
@@ -33,8 +35,16 @@ const Navbar = () => {
         <li className="py-6 text-4xl">Contact</li> 
       </ul>
       {/*Social Menu */}
-      <div className="hidden">
-      className="absolute top-0 left-0 w-4 h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+      <div className="flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center">
+            <a 
+            className="flex justify-between items-center w-full text-gray-300" href="/">
+              Linkedin <FaFacebook size={30}/>
+            </a>
+          </li>
+        </ul>
+      
       </div>
     </div>
   );
