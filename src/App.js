5874feb64950a202b301666f47d481123337e122
dammysaina <dammyadediran94@gmail.com>
2024-05-02 14:55:38 +0100

    skills done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
-
+import Skills from "./components/Skills";
 
 export default function App() {
   return (
     <div>
-      <Navbar/>
-      <Home/>
-     <About/>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
     </div>
-    
   );
 }
-
-
