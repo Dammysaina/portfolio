63d95b6691c0a9c6a29e7316815e89233fd693ac
dammysaina <dammyadediran94@gmail.com>
2024-05-02 01:29:07 +0100

    home fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-pink-600">Hi, my name is </p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Oluwdamilola Adediran</h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Fullstack Developer</h2>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Web Developer</h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
           laudantium, accusantium repellendus quis quam voluptas. Cumque dolor
