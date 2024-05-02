d1abbe9347fcc565873fb1ca434732b4868f7f36
dammysaina <dammyadediran94@gmail.com>
2024-05-03 00:02:07 +0100

    contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <About />
       <Skills />
       <Work />
+      <Contact />
     </div>
   );
 }
