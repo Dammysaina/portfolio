32980c720bae86d26278569ca4b8712c5882b959
dammysaina <dammyadediran94@gmail.com>
2024-05-04 00:18:08 +0100

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form method="POST" action='https://getform.io/f/qaloeoxb' className='flex flex-col max-w-[600px] w-full'>
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+    >
+      <form  action="https://getform.io/f/qaloeoxb" method="POST" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 pt-4'>Submit the form below or shoot me an email - myemail@gmail.com</p>
@@ -13,8 +16,14 @@ const Contact = () => {
             <textarea className='bg-#ccd6f6 p-2' name="message" rows="10" placeholder='Message'></textarea>
             <button className='text-white border-2 hover:bg-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
         </form>
+      {/* <form action="https://getform.io/f/qaloeoxb" method="POST" className="flex flex-col w-full max-w-[600px]">
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <input type="text" name="message" />
+        <button type="submit">Send</button>
+      </form> */}
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
