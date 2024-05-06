92af9b63e48c44da67fd1afd87d27f1f3445401f
dammysaina <dammyadediran94@gmail.com>
2024-05-06 18:38:12 +0100

    update image

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -23,15 +23,15 @@ const Work = () => {
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
+              Intercontinental Maritime and Subsea Service Limited Company's website
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://www.imasslimited.com/home">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/Dammysaina/imass">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
