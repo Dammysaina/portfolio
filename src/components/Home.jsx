1c53f3869dbc1fa3048cd239b7f7c4730e47df9e
dammysaina <dammyadediran94@gmail.com>
2024-05-07 01:55:35 +0100

    logo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
@@ -21,12 +22,15 @@ const Home = () => {
           presence high-quality projects.
         </p>
         <div>
+          <Link to="work">
           <button className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
             View Work
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3" />
             </span>
           </button>
+          </Link>
+          
         </div>
       </div>
     </div>
